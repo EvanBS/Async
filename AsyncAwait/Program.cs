65f5f68942c395bd89c44e323de7d1f282bc292c
EvanBS <evandiesp@gmail.com>
2018-10-23 00:25:21 +0300

    Task

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwait
 {
     class Program
     {
+        // TaskScheduler?
+        static async Task<Int32?> Factorial(Int32? val)
+        {
+            await Task.Factory.StartNew(() => 
+            {
+                Int32? result = 1;
+                while (val != 0)
+                {
+                    result *= val;
+                    val--;
+                }
+                Thread.Sleep(5000); // resource intensive taks imitatoin
+
+                Console.WriteLine($"Factorial is {result}");
+
+                return result;
+
+            });
+
+            return null;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factorial = Factorial(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Further process...");
+            Console.Read();
         }
     }
 }
